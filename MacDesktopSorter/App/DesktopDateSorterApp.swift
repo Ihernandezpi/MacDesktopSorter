4e67bb89cda856d9ce209696dc6ad111450ab8b6
Ignacio Hernández Piña <ignacio.hernandez.pina@gmail.com>
2026-08-31 14:15:36 -0600

    Set final menu bar icon size

--- MacDesktopSorter/App/DesktopDateSorterApp.swift
+++ MacDesktopSorter/App/DesktopDateSorterApp.swift
@@ -79,7 +79,7 @@ struct DesktopDateSorterApp: App {
                     .renderingMode(.template)
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 12, height: 12)
+                    .frame(width: 14, height: 14)
             }
             
         }
